0d8f7b84ad40ecd9e56f0f19fd033e7d9aa992a1
ruby <hs.ruisun@gmail.com>
2018-04-05 08:58:19 -0300

    import functions from another project

---
+++ src/app/form/app-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { LoggerService } from '../logger.service';
+import { User } from "../user.model";
+
+@Component({
+  selector: 'app-hero-form',
+  providers: [LoggerService],
+  templateUrl: './app-form.component.html',
+  styleUrls: ['./app-form.component.css']
+})
+export class AppFormComponent {
+  Category = [{ id: 1, name: 'primary' },
+  { id: 2, name: 'middle' },
+  { id: 3, name: 'high' }];
+  nationalities: string[] = ['Canada', 'China', 'India'];
+  users: User[] = [];
+  user: User = new User('', '', '', 2);
+  
+  constructor(private logger: LoggerService) { }
+
+  onSubmit() {
+    this.users.push(this.user);
+    this.logger.info(this.user.username);
+  }
+}
---
+++ src/app/observable&RxJS/observable.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { interval } from 'rxjs/observable/interval';
+
+
+@Component({
+  selector: 'app-observable',
+  template: ''
+})
+export class ObservableComponent {
+  ngOnInit() {
+    
+  }
+}
